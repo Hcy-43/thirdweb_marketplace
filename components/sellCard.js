aaa4cc62bf4cea08113b472220a18f0f4982ff88
MichaelHsu <b10705014@ntu.edu.tw>
2023-08-15 14:14:20 +0800

    Selling

--- components/sellCard.js
+++ components/sellCard.js
@@ -27,14 +27,13 @@ export default function NftCard({ nftData }) {
                         <Link
                             href={`/sellToken/${NFT_ADDRESS}/${nftData.id}`}
                             key={nftData.id}
-                            className="text-slate-100"
-                        ><div className="transition ease-in-out delay-0 hover:-translate-y-5 hover:scale-105 duration-300 flex flex-col justify-start m-6 basis-1/4 relative overflow-hidden">
-                                <div className='grow relative overflow-hidden'>
-                                    <ThirdwebNftMedia metadata={nftData} height="100%" />
-                                </div >
-                                <div className="font-serif text-5xl p-2 opacity-80">{nftData.name}</div>
-                                <div className="font-serif text-xl ps-2 opacity-50">Not Listed</div>
+                            className="text-slate-100 transition ease-in-out delay-0 hover:-translate-y-5 hover:scale-105 duration-300 flex flex-col justify-start m-6 basis-1/4 relative overflow-hidden"
+                        >
+                            <div className='grow relative overflow-hidden'>
+                                <ThirdwebNftMedia metadata={nftData} height="100%" />
                             </div >
+                            <div className="font-serif text-5xl p-2 opacity-80">{nftData.name}</div>
+                            <div className="font-serif text-xl ps-2 opacity-50">Not Listed</div>
                         </Link>)
             }
 
